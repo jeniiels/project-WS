78269e04e9e8660a849da6de7859ce46b2723a3a
DodowCB <148566647+DodowCB@users.noreply.github.com>
2025-06-17 14:25:31 +0700

    endpoint workouts, logs, subscribe

--- kurusin_backend/src/controllers/apiController.js
+++ kurusin_backend/src/controllers/apiController.js
@@ -1,11 +1,18 @@
-// GET /api/logs
+// GET /api/logs/:username
 const getLogs = async (req, res) => {
-
+    res.json({
+        success: true,
+        message: "This is GET /api/logs/:username endpoint",
+        username: req.params.username
+    });
 };
 
 // POST /api/subscribe
 const subscribe = async (req, res) => {
-
+    res.json({
+        success: true,
+        message: "This is POST /api/subscribe endpoint"
+    });
 };
 
 module.exports = {
--- kurusin_backend/src/controllers/workoutController.js
+++ kurusin_backend/src/controllers/workoutController.js
@@ -2,27 +2,45 @@ const { Workout } = require("../models");
 
 // GET /api/workouts
 const getAll = async (req, res) => {
-
+    res.json({
+        success: true,
+        message: "This is GET /api/workouts endpoint"
+    });
 };
 
 // GET /api/workouts/:id
 const getOne = async (req, res) => {
-
+    res.json({
+        success: true,
+        message: "This is GET /api/workouts/:id endpoint",
+        id: req.params.id
+    });
 };
 
 // POST /api/workouts
 const create = async (req, res) => {
-
+    res.json({
+        success: true,
+        message: "This is POST /api/workouts endpoint"
+    });
 };
 
 // PUT /api/workouts/:id
 const update = async (req, res) => {
-
+    res.json({
+        success: true,
+        message: "This is PUT /api/workouts/:id endpoint",
+        id: req.params.id
+    });
 };
 
 // DELETE /api/workouts/:id
 const remove = async (req, res) => {
-
+    res.json({
+        success: true,
+        message: "This is DELETE /api/workouts/:id endpoint",
+        id: req.params.id
+    });
 };
 
 module.exports = {

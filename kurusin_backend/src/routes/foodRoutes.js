3db72f62985dcc81ffa4aa91196e1e97857dc51b
jenny <jan10cesrebyc2001@gmail.com>
2025-06-26 10:58:48 +0700

    okie

--- kurusin_backend/src/routes/foodRoutes.js
+++ kurusin_backend/src/routes/foodRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { getAll, getOne, create, update, remove } = require('../controllers/foodController');
 const checkApiKey = require('../middlewares/checkApiKey');
-const checkSubscription = require('../middlewares/checkSubscription');
 const updateApiLog = require('../middlewares/updateApiLog');
 const updateApiHit = require('../middlewares/updateApiHit');
+const checkRoles = require('../middlewares/checkRoles');
 
 router.use(updateApiLog);
 router.use(updateApiHit);
@@ -13,10 +13,8 @@ router.get('/', checkApiKey, getAll);
 router.get('/mdp/', getAll);
 router.get('/:id', checkApiKey, getOne);
 router.get('/mdp/:id', getOne);
-
-// POST/PUT/DELETE routes - accessible by Basic & Premium only
-router.post('/', checkApiKey, checkSubscription('basic'), create);
-router.put('/:id', checkApiKey, checkSubscription('basic'), update);
-router.delete('/:id', checkApiKey, checkSubscription('basic'), remove);
+router.post('/', create);
+router.put('/:id', checkApiKey, checkRolesoles("admin"), update);
+router.delete('/:id', checkApiKey, checkRoles("admin"), remove);
 
 module.exports = router;
